3daac0f78b71b9d4db0ce4b3788d45ffdde5b272
un_phoque <unphoque38@gmail.com>
2021-08-27 18:34:34 +0200

    Added Toornament tokens

--- bot.js
+++ bot.js
@@ -1,16 +1,23 @@
-const config = require("./config.json")
-const friendCodes = require("./friend_codes.json")
-
+//External libraries
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 const XHR = require("xmlhttprequest").XMLHttpRequest;
 
-const login=function(nbTries=0) {
+//Internal libraries
+const config = require("./config.json");
+const friendCodes = require("./friend_codes.json");
+
+//Environment variables
+process.env.TOORNAMENT_TOKEN_RESULT = "";
+process.env.TOORNAMENT_TOKEN_PARTICIPANT = "";
+
+//Functions
+const login = function(nbTries = 0) {
     client.login(config.DISCORD_TOKEN)
         .catch((err) => {
             if (nbTries == 5) {
-                console.error("Error: cannot connect to client.")
+                console.error("Error: cannot connect to client.");
             }
             else {
                 console.error("Retrying to connect...");
@@ -19,9 +26,51 @@ const login=function(nbTries=0) {
         });
 }
 
+const getToornamentAuthorizations = function () {
+    for (let scope in ["result", "participant"]) {
+        const req = new XHR();
+        var url = "https://api.toornament.com/oauth/v2/token";
+        req.open("POST", url);
+        req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        req.addEventListener("load", function () {
+            if (req.status < 200 && req.status >= 400)
+                console.error(req.status + " " + req.statusText + " " + url);
+        });
+        req.addEventListener("error", function () {
+            console.error("Error with URL " + url);
+        });
+        req.addEventListener("readystatechange", function () {
+            if (req.readyState === 4) {
+                let res = JSON.parse(req.responseText)
+                let token = res.access_token
+                switch (scope) {
+                    case "result":
+                        process.env.TOORNAMENT_TOKEN_RESULT = token;
+                        break;
+                    case "participant":
+                        process.env.TOORNAMENT_TOKEN_PARTICIPANT = token;
+                        break;
+                }
+            }
+        });
 
+        req.send(
+            "grant_type=client_credentials&client_id=" +
+            config.TOORNAMENT_ID +
+            "&client_secret=" +
+            config.TOORNAMENT_SECRET +
+            "&scope=organizer:" +
+            scope
+        );
+    }
+}
+
+//Event listeners
 client.on('ready', () => console.log(`Logged in as ${client.user.tag}!`)); //Detect when the bot is logged in.
 
 client.on('error', console.error); //Avoid killing the bot if an error occur.
 
-login(); //Log the bot into Discord. Must be called as last instruction.
+//Instructions
+login(); //Log the bot into Discord.
+
+setInterval(getToornamentAuthorizations, 86400000); //Refresh Toornament tokens when they expire
