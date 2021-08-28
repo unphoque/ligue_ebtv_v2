96fa8d49bedd685a97ad7cf0776535b70ae0baa9
koro <me@krbk.dev>
2021-08-28 15:19:02 +0200

    :sparkles: Starting help command

---
+++ commands/help.js
@@ -0,0 +1,11 @@
+const { Collection } = require('discord.js');
+
+module.exports = {
+    run: (client, message) => {
+        let a = new Collection();
+    },
+    name: "help",
+    aliases: [],
+    category: "Utilitaires",
+    description: "Envoie le menu d'aide."
+}
