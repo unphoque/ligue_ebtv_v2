fd93ed7471a38b2f391798e63e0d66b6bf0c89fd
koro <me@krbk.dev>
2021-08-28 20:23:08 +0200

    :sparkles: Introduce help command

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 //External libraries
 const { Client, Intents, Collection } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 
 const XHR = require("xmlhttprequest").XMLHttpRequest;
 
--- commands/help.js
+++ commands/help.js
@@ -1,8 +1,31 @@
-const { Collection } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     run: (client, message) => {
-        let a = new Collection();
+        let categories = {};
+        let categoriesnames = [];
+        client.commands.each(cmd => {
+            if (!categories[cmd.category]) {
+                categories[cmd.category] = [[cmd.name, cmd.description]];
+                categoriesnames.push(cmd.category);
+            } else {
+                categories[cmd.category].push([cmd.name, cmd.description]);
+            }
+        });
+
+        const e = new MessageEmbed();
+
+        e.setTitle("Menu d'aide").setDescription("_ _");
+
+        for (const cn of categoriesnames) {
+            e.addField(`${cn}:`, "_ _");
+            for (const cmd of categories[cn]) {
+                e.addField(`!${cmd[0]}`, cmd[1]);
+            }
+        }
+
+        e.setFooter(`Demandé par ${message.author.tag}`, message.author.avatarURL());
+        message.reply({embeds: [e]});
     },
     name: "help",
     aliases: [],
